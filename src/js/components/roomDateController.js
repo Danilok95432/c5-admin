1d2ecd006ca168291eb0d90609b88d1570c68f84
luspi1 <47425618+luspi1@users.noreply.github.com>
2023-08-14 17:33:54 +0300

    room-chain: шапка календаря

--- src/js/components/roomDateController.js
+++ src/js/components/roomDateController.js
@@ -1,23 +1,37 @@
 import AirDatepicker from "air-datepicker";
-import moment from "moment";
+import moment from "moment/moment";
 
 const roomDateController = document.querySelector('.room-date-controller')
 
 if (roomDateController) {
   const calendarInput = roomDateController.querySelector('.date-mask')
+  const datePreview = roomDateController.querySelector('.room-date-controller__date-preview')
+  const switcherDate = roomDateController.querySelector('.room-date-controller__switcher-date')
+
 
   const prevCalendarBtn = roomDateController.querySelector('.room-date-controller__switcher-btn.prev-btn')
   const nextCalendarBtn = roomDateController.querySelector('.room-date-controller__switcher-btn.next-btn')
 
-  const customRoomCalendar = new AirDatepicker(calendarInput)
+  const customRoomCalendar = new AirDatepicker(calendarInput, {
+    onSelect: ({date, formattedDate}) => {
+      datePreview.textContent = moment(date).format('MMMM YYYY');
+      const nowDate = moment().format('L');
 
-  let dateCount = 0
+
+      if (formattedDate === nowDate) {
+        switcherDate.textContent = 'сегодня'
+      } else {
+        switcherDate.textContent = formattedDate
+      }
+
+    }
+  })
+
 
   const getInputDate = (input) => {
     if (input.value) {
       const formatDate = input.value.split(".").reverse().join("-")
       const resDate = new Date(formatDate)
-
       return resDate
     } else {
       return new Date()
@@ -25,15 +39,15 @@ if (roomDateController) {
   }
 
   nextCalendarBtn.addEventListener('click', () => {
-    let nextDate = moment(getInputDate(calendarInput)).add(dateCount, 'days')._d;
-    dateCount++
-
+    const currentDate = getInputDate(calendarInput)
+    let nextDate = currentDate.setDate(currentDate.getDate() + 1)
     customRoomCalendar.selectDate(nextDate)
+    console.log()
   })
   prevCalendarBtn.addEventListener('click', () => {
-    dateCount--
-    let currentDate = customRoomCalendar.selectedDates[0]
-    let nextDate = moment(currentDate).add(dateCount, 'days')._d;
-    customRoomCalendar.selectDate(nextDate)
+    const currentDate = getInputDate(calendarInput)
+    let prevDate = currentDate.setDate(currentDate.getDate() - 1)
+    customRoomCalendar.selectDate(prevDate)
   })
+
 }

6deffae68473ec7f4582c7e4822ea1be728b60c0
luspi1 <47425618+luspi1@users.noreply.github.com>
2023-09-13 17:23:05 +0300

    edit-booking.html: реализована логика селектов с количеством гостей, rooms-apartment: небольшие изменения по разметке, для приведения к обще логике селектов-генераторов, настройка eslint+prettier

--- src/js/components/confirmBtns.js
+++ src/js/components/confirmBtns.js
@@ -1,7 +1,7 @@
 const confirmBtns = document.querySelectorAll('[data-btn="confirm"]')
 
 if (confirmBtns) {
-  confirmBtns.forEach(btn => {
+  confirmBtns.forEach((btn) => {
     btn.addEventListener('click', (e) => {
       e.preventDefault()
       const hrefLink = e.target?.href
--- src/js/components/cutStrings.js
+++ src/js/components/cutStrings.js
@@ -1,8 +1,7 @@
-
 export const cutString = (stringArray, stringLength) => {
-  stringArray.forEach(str => {
+  stringArray.forEach((str) => {
     let cutLength = 0
-    stringLength ? cutLength = stringLength : cutLength = +str.dataset.shear
+    stringLength ? (cutLength = stringLength) : (cutLength = +str.dataset.shear)
 
     if (cutLength && cutLength < str.textContent.length) {
       str.textContent = `${str.textContent.substring(0, cutLength)}...`
@@ -10,5 +9,5 @@ export const cutString = (stringArray, stringLength) => {
   })
 }
 
-const truncatedStrings =  document.querySelectorAll('[data-shear]')
+const truncatedStrings = document.querySelectorAll('[data-shear]')
 cutString(truncatedStrings)

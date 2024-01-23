71b8df4b1ae5b35db5ef8cbb6077e2af0b618d83
luspi1 <47425618+luspi1@users.noreply.github.com>
2024-01-23 12:55:26 +0300

    edit-booking.html: частичная реализация скриптов для поиска номеров

--- src/js/components/editBooking.js
+++ src/js/components/editBooking.js
@@ -1,11 +1,32 @@
 import { newAdultGuest, newChildGuest, newRoom } from '../_vars'
 import { initAllDates } from './customDate'
+import { sendData, showInfoModal } from '../_functions'
 
 const editBookingPage = document.querySelector('.edit-booking-page')
 
 if (editBookingPage) {
   const roomsWrapper = editBookingPage.querySelector('.rooms-category')
   const roomsList = editBookingPage.querySelector('.rooms-list')
+  const editBookingForm = editBookingPage.querySelector(
+    '.edit-booking-page__form',
+  )
+
+  const searchRoomsBtn = editBookingPage.querySelector(
+    '.rooms-params__search-btn',
+  )
+  const freeRoomsList = editBookingPage.querySelector('.rooms-params__list')
+  const roomsCategoriesSelectWrapper = editBookingPage.querySelector(
+    '.input-column-wrapper__categories-select',
+  )
+  const roomsTariffsSelectWrapper = editBookingPage.querySelector(
+    '.input-column-wrapper__tariff-select',
+  )
+
+  const roomsSaveBtn = editBookingPage.querySelector(
+    '.rooms-params__btn-wrapper',
+  )
+
+  // Логика добавления номеров и гостей с нумерацией имен инпутов
 
   let observer = new MutationObserver(() => {
     ;[...roomsList.children].forEach((room, roomIdx) => {
@@ -22,6 +43,7 @@ if (editBookingPage) {
         })
       })
     })
+    roomsSaveBtn.classList.add('_blocked')
   })
 
   observer.observe(roomsList, {
@@ -54,4 +76,87 @@ if (editBookingPage) {
       initAllDates()
     }
   })
+
+  // Логика поиска подходящих номеров
+  const initBlockedInputs = () => {
+    const blockedInputs = editBookingPage.querySelectorAll(
+      '._block-search-input',
+    )
+    blockedInputs.forEach((el) => {
+      el.addEventListener('input', () => {
+        roomsSaveBtn.classList.add('_blocked')
+      })
+    })
+  }
+
+  const searchScript = searchRoomsBtn.dataset.script
+
+  searchRoomsBtn.addEventListener('click', async () => {
+    if (!editBookingForm.checkValidity()) {
+      const invalidElements = editBookingForm.querySelectorAll(':invalid')
+      invalidElements[0].reportValidity()
+      return
+    }
+
+    const formData = new FormData(editBookingForm)
+    const data = Object.fromEntries(formData.entries())
+
+    const jsonData = JSON.stringify(data)
+
+    try {
+      const response = await sendData(jsonData, searchScript)
+      const finishedResponse = await response.json()
+
+      const { status, errortext, htmlRooms, htmlCategories, htmlTariffs } =
+        finishedResponse
+      if (status === 'ok') {
+        roomsSaveBtn.classList.remove('_blocked')
+        freeRoomsList.innerHTML = htmlRooms
+        roomsCategoriesSelectWrapper.innerHTML = htmlCategories
+        roomsTariffsSelectWrapper.innerHTML = htmlTariffs
+        initBlockedInputs()
+        initRoomsCategories()
+      } else {
+        showInfoModal(errortext)
+      }
+    } catch (err) {
+      console.error(err)
+      showInfoModal('Во время выполнения запроса произошла ошибка')
+    }
+  })
+
+  const initRoomsCategories = () => {
+    const roomsCategoriesScript = roomsCategoriesSelectWrapper.dataset.script
+
+    const roomsCategoriesSelect =
+      roomsCategoriesSelectWrapper.querySelector('.main-input')
+
+    roomsCategoriesSelect.addEventListener('input', async (e) => {
+      const data = {
+        room_category: e.currentTarget.value,
+      }
+
+      const jsonData = JSON.stringify(data)
+
+      try {
+        const response = await sendData(jsonData, roomsCategoriesScript)
+        const finishedResponse = await response.json()
+
+        const { status, errortext, htmlRooms, htmlTariffs } = finishedResponse
+        if (status === 'ok') {
+          freeRoomsList.innerHTML = htmlRooms
+          roomsTariffsSelectWrapper.innerHTML = htmlTariffs
+          initBlockedInputs()
+        } else {
+          showInfoModal(errortext)
+        }
+      } catch (err) {
+        console.error(err)
+        showInfoModal('Во время выполнения запроса произошла ошибка')
+      }
+    })
+  }
+
+  initRoomsCategories()
+  initBlockedInputs()
 }

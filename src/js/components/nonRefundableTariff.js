1d8e985eab766df55a8d0da7bd139ac251708a6a
ludaalt <38361378+ludaalt@users.noreply.github.com>
2024-05-17 10:36:13 +0300

    корректировки

---
+++ src/js/components/nonRefundableTariff.js
@@ -0,0 +1,11 @@
+const nonRefundableCheckbox = document.querySelector('[name="non-refundable"]')
+
+const conditionBlocks = document.querySelectorAll('.condition-block')
+
+nonRefundableCheckbox.addEventListener('click', () => {
+  if (nonRefundableCheckbox.checked) {
+    conditionBlocks.forEach((item) => (item.style.display = 'none'))
+  } else {
+    conditionBlocks.forEach((item) => (item.style.display = 'flex'))
+  }
+})

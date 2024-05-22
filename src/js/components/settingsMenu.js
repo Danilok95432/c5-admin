bc208a1495692641fda276e4c4bf01c53dc1088b
luspi1 <47425618+luspi1@users.noreply.github.com>
2024-05-22 17:51:21 +0300

    правка

--- src/js/components/settingsMenu.js
+++ src/js/components/settingsMenu.js
@@ -4,13 +4,13 @@ if (authPanel) {
   const settingsMenu = authPanel.querySelector('.settings-menu')
   const settingsBtn = authPanel.querySelector('.auth-panel__settings')
 
-  authPanel.addEventListener('click', (e) => {
+  authPanel?.addEventListener('click', (e) => {
     if (e.target.classList.contains('settings-menu__overlay')) {
       settingsMenu.classList.remove('_active')
     }
   })
 
-  settingsBtn.addEventListener('click', () => {
+  settingsBtn?.addEventListener('click', () => {
     settingsMenu.classList.toggle('_active')
   })
 }

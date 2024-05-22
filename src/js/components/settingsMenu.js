6dcc4f0a75e66858b3babe9352371ed3f58749d8
luspi1 <47425618+luspi1@users.noreply.github.com>
2024-05-22 17:45:04 +0300

    выпадающие меню, версия модалки трека с новым бронированием

---
+++ src/js/components/settingsMenu.js
@@ -0,0 +1,16 @@
+const authPanel = document.querySelector('.auth-panel')
+
+if (authPanel) {
+  const settingsMenu = authPanel.querySelector('.settings-menu')
+  const settingsBtn = authPanel.querySelector('.auth-panel__settings')
+
+  authPanel.addEventListener('click', (e) => {
+    if (e.target.classList.contains('settings-menu__overlay')) {
+      settingsMenu.classList.remove('_active')
+    }
+  })
+
+  settingsBtn.addEventListener('click', () => {
+    settingsMenu.classList.toggle('_active')
+  })
+}

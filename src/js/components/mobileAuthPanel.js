a60921dfd333be1ac687fa1eb0c79855617bd9e4
luspi1 <igbabin566@gmail.com>
2024-09-30 18:31:11 +0300

    адаптив  шапки, правки

--- src/js/components/mobileAuthPanel.js
+++ src/js/components/mobileAuthPanel.js
@@ -1,6 +1,14 @@
 const mobileAuthPanel = document.querySelector('.mobile-auth-panel')
 
 if (mobileAuthPanel) {
+  const removeOpenClass = (...elementsArr) => {
+    elementsArr.forEach((el) => {
+      if (el.classList.contains('_open')) {
+        el.classList.remove('_open')
+      }
+    })
+  }
+
   // Логика бургер меню
   const burgerBtn = mobileAuthPanel.querySelector(
     '.mobile-auth-panel__burger-btn',
@@ -8,6 +16,7 @@ if (mobileAuthPanel) {
   const mobileMenu = mobileAuthPanel.querySelector('.mobile-auth-menu')
 
   burgerBtn?.addEventListener('click', () => {
+    removeOpenClass(settingsBtn, settingsMenu, noticeBtn, noticeMenu)
     burgerBtn.classList.toggle('_open')
     mobileMenu.classList.toggle('_open')
   })
@@ -18,7 +27,18 @@ if (mobileAuthPanel) {
   const settingsMenu = mobileAuthPanel.querySelector('.settings-auth-menu')
 
   settingsBtn?.addEventListener('click', () => {
+    removeOpenClass(burgerBtn, mobileMenu, noticeBtn, noticeMenu)
     settingsBtn.classList.toggle('_open')
     settingsMenu.classList.toggle('_open')
+  })
+
+  // Логика меню оповещений
+  const noticeBtn = mobileAuthPanel.querySelector('.mobile-auth-panel__notice')
+  const noticeMenu = mobileAuthPanel.querySelector('.mobile-notice-menu')
+
+  noticeBtn?.addEventListener('click', () => {
+    removeOpenClass(burgerBtn, mobileMenu, settingsBtn, settingsMenu)
+    noticeBtn.classList.toggle('_open')
+    noticeMenu.classList.toggle('_open')
   })
 }
--- src/js/components/mobileSelects.js
+++ src/js/components/mobileSelects.js
@@ -1,17 +1,60 @@
 const mobileSelects = document.querySelectorAll('.mobile-select')
 
 if (mobileSelects?.length) {
-  mobileSelects.forEach((el) => {
-    const currentSelect = el.querySelector('select')
-    currentSelect.addEventListener('click', (e) => {
-      const optionsArray = Array.from(e.currentTarget.options).map(
-        (option) => ({
-          text: option.text,
-          value: option.value,
-          selected: option.selected, // Это поле будет true, если option выбран
-        }),
+  const openedMenu = document.querySelector('.opened-select-menu')
+
+  openedMenu.addEventListener('click', (e) => {
+    const currentId = openedMenu.dataset.id
+    if (e.target.classList.contains('opened-select-menu__close-btn')) {
+      openedMenu.classList.remove('_active')
+    }
+    if (e.target.dataset.value) {
+      const currentSelect = document.querySelector(
+        `.mobile-select[data-select-id=${currentId}] select`,
       )
-      console.log(optionsArray)
+      currentSelect.value = e.target.dataset.value
+      openedMenu.classList.remove('_active')
+      if (currentSelect.closest('.auto-submit-form')) {
+        const parentForm = currentSelect.closest('.auto-submit-form')
+        parentForm.submit()
+      }
+    }
+  })
+
+  const renderMobileSelect = (selectsArr, labelText, openedMenu, selectId) => {
+    openedMenu.innerHTML = ''
+    const selectElements = selectsArr
+      .map(
+        ({ text, value, selected }) => `
+      <li data-value="${value}" data-selected="${selected}"><p>${text}</p></li>
+    `,
+      )
+      .join('')
+    const resultMarkup = `
+      <button class="opened-select-menu__close-btn" type="button">
+      </button>
+      <h6>${labelText}</h6>
+      <ul>${selectElements}</ul>
+    `
+    openedMenu.insertAdjacentHTML('beforeend', resultMarkup)
+    openedMenu.dataset.id = selectId
+    openedMenu.classList.add('_active')
+  }
+
+  mobileSelects.forEach((el) => {
+    const selectLabelText = el.querySelector('label').textContent
+    const currentSelect = el.querySelector('select')
+    const selectId = el.dataset.selectId
+
+    currentSelect.addEventListener('click', (e) => {
+      e.preventDefault()
+      const selectArr = Array.from(e.currentTarget.options).map((option) => ({
+        text: option.text,
+        value: option.value,
+        selected: option.selected,
+      }))
+      renderMobileSelect(selectArr, selectLabelText, openedMenu, selectId)
     })
+    currentSelect.addEventListener('mousedown', (e) => e.preventDefault())
   })
 }

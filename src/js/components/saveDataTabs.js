482b1ebc40e96c8adcbf1baf51f67f8d1a8ec058
luspi1 <igbabin566@gmail.com>
2024-10-03 16:56:42 +0300

    правки скрипта сохранения данных в категориях

--- src/js/components/saveDataTabs.js
+++ src/js/components/saveDataTabs.js
@@ -2,12 +2,11 @@ const saveDataTabMenu = document.querySelector('.tab-menu._save-data')
 
 if (saveDataTabMenu) {
   const saveDataLinks = saveDataTabMenu.querySelectorAll('a[data-script]')
-  const activeLink = saveDataTabMenu.querySelector('a._active')
-  const currentScript = activeLink.dataset.script
 
   saveDataLinks.forEach((linkEl) => {
     linkEl.addEventListener('click', (e) => {
       e.preventDefault()
+      const currentScript = e.currentTarget.dataset.script
       if (!linkEl.classList.contains('_active')) {
         const currentForm = document?.querySelector('main form')
         if (currentForm) {

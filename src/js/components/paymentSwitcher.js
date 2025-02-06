3fe2fae67c69f4b455ffe97f7bac042c546ecf04
Danilok95432 <danila.dvoretskov@yandexe.ru>
2025-02-06 12:37:12 +0300

    правки скрипта платежных настроек

--- src/js/components/paymentSwitcher.js
+++ src/js/components/paymentSwitcher.js
@@ -2,19 +2,28 @@ const buttons = document.querySelectorAll('.payment-switch-mode button')
 const hiddenInput = document.querySelector(
   '.payment-switch-mode input[type="hidden"]',
 )
-const paymentDesc = document.querySelector('.payment-desc')
+const paymentDesc = document.querySelectorAll('.payment-desc')
 
 if (buttons && hiddenInput && paymentDesc) {
   buttons.forEach((button) => {
     button.addEventListener('click', () => {
+      if (button.classList.contains('_active')) {
+        return
+      }
+
       buttons.forEach((b) => b.classList.remove('_active'))
       button.classList.add('_active')
+
       if (button.textContent.includes('Тестовый')) {
         hiddenInput.value = '0'
-        paymentDesc.innerHTML = `Сейчас включен тестовый режим, и Вы <span> не можете </span>принимать платежи от реальных клиентов`
+        paymentDesc.forEach((desc) =>
+          desc.classList.toggle('_active', desc === paymentDesc[0]),
+        )
       } else {
         hiddenInput.value = '1'
-        paymentDesc.innerHTML = `Сейчас включен коммерческий режим, и <span class="afford">Вы можете</span> принимать платежи от реальных клиентов`
+        paymentDesc.forEach((desc) =>
+          desc.classList.toggle('_active', desc === paymentDesc[1]),
+        )
       }
     })
   })
